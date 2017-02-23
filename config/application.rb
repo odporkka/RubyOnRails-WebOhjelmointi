9cb66d7e1f1c7c85151782cde514bfee6cbb9f32
Otto Porkka <ode.porkka@gmail.com>
2017-02-23 23:27:39 +0200

    Assets fix for Heroku.

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,7 @@ module Ratebeer
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += Dir["#{Rails.root}/lib"]
+
+    config.serve_static_assets = true
   end
 end
